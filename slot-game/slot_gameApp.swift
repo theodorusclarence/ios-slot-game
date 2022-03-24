c3e6d72afb3beef42e785c55f7c6d0d0955044f0
Theodorus Clarence <theodorusclarence@gmail.com>
2022-03-24 23:13:32 +0700

    Initial Commit

---
+++ slot-game/slot_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_gameApp.swift
+//  slot-game
+//
+//  Created by Clarence on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct slot_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
